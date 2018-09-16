5109e98b629df55b2b2efa58cde12acaf60d0d34
Paul Hebert <paul.hebert@paulhebertdesigns.com>
2018-09-16 12:59:28 -0700

    More info about web colors

--- php/work.php
+++ php/work.php
@@ -2,7 +2,7 @@
 	$work = [
 				[
 					'Colors of the Web',
-					'By displaying the colors used in popular sites in a variety of ways we can glean interesting conclusions about modern trends in web design. Organizing the colors by hue, saturation and value provides viewers with a variety of different ways to view the data. Instead of forcing conclusions upon the user this visualization provides them with the information they need to draw their own insights. I scraped the data using PHP, then built the page using PHP, HTML, SCSS, and Javascript.',
+					'By displaying the colors used in popular sites in a variety of ways we can glean interesting conclusions about modern trends in web design. Organizing the colors by hue, saturation and value provides viewers with a variety of different ways to view the data. Instead of forcing conclusions upon the user this visualization provides them with the information they need to draw their own insights. I scraped the data using PHP, then built the page using PHP, HTML, SCSS, and Javascript. This visualization was viewed by over 60,000 unique viewers and featured in dozens of publications including the <a href="https://www.washingtonpost.com/news/the-intersect/wp/2016/09/14/can-you-guess-the-website-based-on-the-swatch-of-blue/" target="_BLANK">Washington Post</a>, <a href="https://www.wired.com/2016/09/popular-color-internet/" target="_BLANK">Wired Magazine</a>, <a href="https://blogs.adobe.com/creativecloud/cool-or-just-common-blue-is-the-webs-most-popular-color/?trackingid=FD1KZNVN&mv=email" target="_BLANK">Adobe</a>, and many more.',
 					'/web_colors/',
 					'wc1',
 					[
